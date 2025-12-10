a520c15bfbb1fdb8cfade27c6d2b17c1351dc744
vladiwanov <3298901@gmail.com>
2025-12-10 10:34:54 +0200

    first comit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import GameModule from './components/GameModule'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        <GameModule/>
+</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GameModule/GameModule.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function GameModule() {
+  return <div>
+    
+    <h1>
+      Hi it's Game Module
+    </h1>
+    
+    Hi</div>;
+}
